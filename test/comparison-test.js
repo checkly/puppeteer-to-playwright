bbeb38dbdeafc70bdaea45d01b33d4ddf5f24f61
ragog <giovanni@checklyhq.com>
2021-12-28 15:58:30 +0100

    feat: tests

---
+++ test/comparison-test.js
@@ -0,0 +1,31 @@
+const { promises: fs } = require("fs")
+const nfs = require ("fs")
+const path = require("path")
+const assert = require("chai").assert;
+
+const dirOut = nfs.readdirSync('test/output')
+
+for (dir of dirOut) {
+    const scriptTransformed = nfs.readFileSync(`test/output/${dir}`)
+    const scriptExpected = nfs.readFileSync(`test/expected/${dir}`)
+    console.log('Comparing ' + dir)
+    assert(scriptTransformed.equals(scriptExpected))
+}
+
+console.log('Tests ran')
+
+copyDir('./test/base', './test/output');
+
+async function copyDir(src, dest) {
+    await fs.mkdir(dest, { recursive: true });
+    let entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let srcPath = path.join(src, entry.name);
+        let destPath = path.join(dest, entry.name);
+
+        entry.isDirectory() ?
+            await copyDir(srcPath, destPath) :
+            await fs.copyFile(srcPath, destPath);
+    }
+}
---
+++ test/expected/puppeteer-vue.js
@@ -0,0 +1,20 @@
+const { chromium } = require('playwright');
+
+(async () => {
+    const browser = await chromium.launch({headless: false})
+    const page = await browser.newPage()
+
+    await page.goto('https://v3.vuejs.org/')
+
+    await page.setViewportSize({ width: 1680, height: 887 })
+
+    await page.click('.hero > .inner > .right > .actions > .nav-link:nth-child(2)')
+
+    await page.click('.theme-container > .page > .theme-default-content > p > .nav-link')
+
+    await page.click('.page > .theme-default-content > ol > li:nth-child(4) > a')
+
+    await page.click('.theme-container > .page > .theme-default-content > p:nth-child(31) > a:nth-child(1)')
+
+    await browser.close()
+})()
